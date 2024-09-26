6ccbebeea282d565b2d5fcc749f9d131069f4f04
aslifisika <jajangkunaedi74@guru.sma.belajar.id>
2024-09-26 11:38:29 +0700

    fikasu

---
+++ story_content/user.js
@@ -0,0 +1,348 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6gWnT8jC1eK":
+        Script1();
+        break;
+      case "5lkhSsPpIEG":
+        Script2();
+        break;
+      case "69wutNYXXza":
+        Script3();
+        break;
+      case "6onEKJ8F2uC":
+        Script4();
+        break;
+      case "66PpKzEqEJB":
+        Script5();
+        break;
+      case "6ZyDuuW6TEZ":
+        Script6();
+        break;
+      case "6XgUQCa9vWK":
+        Script7();
+        break;
+      case "6RyxETT3h4n":
+        Script8();
+        break;
+      case "5aA7KvguGLv":
+        Script9();
+        break;
+      case "5pmIFATyo0T":
+        Script10();
+        break;
+      case "5WWYfb8ugKH":
+        Script11();
+        break;
+      case "622o91FEJ1C":
+        Script12();
+        break;
+      case "6OUysdeP3op":
+        Script13();
+        break;
+      case "61wVMkD0cju":
+        Script14();
+        break;
+      case "6Nqkk66OKH3":
+        Script15();
+        break;
+      case "61Gu7FNMqTH":
+        Script16();
+        break;
+      case "6N9yHeOIe0M":
+        Script17();
+        break;
+      case "6Fb3vLAxbaL":
+        Script18();
+        break;
+      case "6EgoADBlrrP":
+        Script19();
+        break;
+      case "6WdUPhpSUfR":
+        Script20();
+        break;
+      case "6CtknBbC63a":
+        Script21();
+        break;
+      case "5d3V42cZXbD":
+        Script22();
+        break;
+      case "6HwLTSUIMKp":
+        Script23();
+        break;
+      case "60ixLBB9fqe":
+        Script24();
+        break;
+      case "5hP6fgibqqT":
+        Script25();
+        break;
+      case "5b8qtcWcqXI":
+        Script26();
+        break;
+      case "5ZSHvP1rXLT":
+        Script27();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku'); audio.src="musikku.mp3";
+audio.load(); 
+audio.play(); 
+audio.volume=0.5;
+}
+
+function Script2()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.0;
+
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.5;
+
+}
+
+function Script5()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script6()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script7()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script8()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script9()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script10()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script11()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script12()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script13()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script14()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script15()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script16()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script17()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script18()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script19()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script20()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script21()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script22()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script23()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script24()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script25()
+{
+  function toggleMute() {
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0);
+}
+toggleMute();
+
+}
+
+function Script26()
+{
+  var currentDate = new Date()
+var day = currentDate.getDate()
+var month = currentDate.getMonth() + 1
+var year = currentDate.getFullYear();
+var player = GetPlayer();
+var newName = day + "/" + month + "/" +year
+player.SetVar("DateValue", newName);
+
+}
+
+function Script27()
+{
+  window.print(); 
+}
+
